74173d08f19ebd2935b1cea528ba942a315771e8
Aria Stewart <aredridel@dinhe.net>
2014-12-02 15:27:18 -0500

    Initial stab

---
+++ index.js
@@ -0,0 +1,48 @@
+(function () {
+    "use strict";
+
+    function registerWith(dust) {
+        if (dust.addLoadMiddleware) return;
+
+        var originalLoader = dust.onLoad;
+        var middlewares = [];
+
+        dust.addLoadMiddleware = addLoadMiddleware;
+
+        function addLoadMiddleware(middleware) {
+            middlewares.push(middleware);
+        }
+
+        dust.onLoad = load;
+
+        function load(name, cb) {
+            var toRun = middlewares.slice();
+            if (originalLoader) toRun.push(originalLoader);
+
+            function runChain() {
+                var handler = toRun.shift();
+                if (!handler) {
+                    return cb(new Error("No template found named '" + name + "'"));
+                }
+
+                handler(name, function (err, data) {
+                    if (err) {
+                        return cb(err);
+                    } else if (data) {
+                        return cb(null, data);
+                    } else {
+                        runChain();
+                    }
+                });
+            }
+
+            runChain();
+        }
+    }
+
+    if (typeof module == 'undefined') {
+        registerWith(dust);
+    } else {
+        module.exports.registerWith = registerWith;
+    }
+})();

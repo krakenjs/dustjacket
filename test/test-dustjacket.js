d2ceeb7ceedfcebe584e1f1cd766bbedaae0f747
Aria Stewart <aredridel@dinhe.net>
2014-12-02 16:03:05 -0500

    tests!

---
+++ test/test-dustjacket.js
@@ -0,0 +1,71 @@
+var test = require('tape');
+var freshy = require('freshy').freshy;
+var dustjacket = require('../');
+
+test('registration', function (t) {
+    var dust = freshy('dustjs-linkedin');
+    var load = dust.onLoad;
+    dustjacket.registerWith(dust);
+
+    t.notEqual(load, dust.onLoad, 'loader was replaced');
+    t.ok(dust.addLoadMiddleware, 'addLoadMiddleware method was added');
+
+    t.end();
+});
+
+test('middleware can pass', function (t) {
+    var dust = freshy('dustjs-linkedin');
+
+    dustjacket.registerWith(dust);
+
+    t.plan(3);
+
+    dust.addLoadMiddleware(function (name, cb) {
+        t.pass('called');
+        cb();
+    });
+
+    dust.render('test', {}, function (err, out) {
+        t.equal(err.message, "No template found named 'test'");
+        t.notOk(out);
+        t.end();
+    });
+});
+
+test('middleware can error', function (t) {
+    var dust = freshy('dustjs-linkedin');
+
+    dustjacket.registerWith(dust);
+
+    t.plan(3);
+
+    dust.addLoadMiddleware(function (name, cb) {
+        t.pass('called');
+        cb('error');
+    });
+
+    dust.render('test', {}, function (err, out) {
+        t.equal(err, 'error');
+        t.notOk(out);
+        t.end();
+    });
+});
+
+test('middleware can return data', function (t) {
+    var dust = freshy('dustjs-linkedin');
+
+    dustjacket.registerWith(dust);
+
+    t.plan(3);
+
+    dust.addLoadMiddleware(function (name, cb) {
+        t.pass('called');
+        cb(null, 'data');
+    });
+
+    dust.render('test', {}, function (err, out) {
+        t.error(err);
+        t.equal(out, 'data');
+        t.end();
+    });
+});
